859c799d95a5fadcdcc09ce475809296a1dc85b5
shatteredmoon <jbwoodbrooks@gmail.com>
2025-10-13 22:51:31 -0400

    Lecture 11: Improving Bot Animations & C++ Asserts

--- Source/action_roguelike/Private/AI/SAIController.cpp
+++ Source/action_roguelike/Private/AI/SAIController.cpp
@@ -10,7 +10,10 @@ void ASAIController::BeginPlay()
 {
   Super::BeginPlay();
 
-  RunBehaviorTree( BehaviorTree );
+  if( ensureMsgf( BehaviorTree, TEXT( "Behavior Tree is nullptr! Please assign BehaviorTree in your AI Controller." ) ) )
+  {
+    RunBehaviorTree( BehaviorTree );
+  }
 
   // Now using SAICharacter::OnPawnSeen
   /*APawn* MyPawn{UGameplayStatics::GetPlayerPawn(this, 0)};
