6bf314e45388b764070814f06395da562f694404
shatteredmoon <jbwoodbrooks@gmail.com>
2025-12-19 21:53:40 -0500

    Lecture 23: Serializing Blueprints & Saving via Actor Interaction Adds a bonfire whose state is saved

--- Source/action_roguelike/Public/SGameplayInterface.h
+++ Source/action_roguelike/Public/SGameplayInterface.h
@@ -6,11 +6,12 @@
 #include "UObject/Interface.h"
 #include "SGameplayInterface.generated.h"
 
+
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI)
 class USGameplayInterface : public UInterface
 {
-	GENERATED_BODY()
+  GENERATED_BODY()
 };
 
 /**
@@ -18,15 +19,15 @@ class USGameplayInterface : public UInterface
  */
 class ACTION_ROGUELIKE_API ISGameplayInterface
 {
-	GENERATED_BODY()
+  GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+  // Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 
   // Called after the Actor state was restored from a SaveGame file
-  UFUNCTION( BlueprintNativeEvent )
+  UFUNCTION( BlueprintCallable, BlueprintNativeEvent )
   void OnActorLoaded();
 
-  UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+  UFUNCTION( BlueprintCallable, BlueprintNativeEvent )
   void Interact( APawn* InstigatorPawn );
 };
